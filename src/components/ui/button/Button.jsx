92bd00d5ccb5b559673e86080d6f6adfcc8667ed
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-21 16:49:45 -0300

    feat(components): add Button component

---
+++ src/components/ui/button/Button.jsx
@@ -0,0 +1,20 @@
+import styles from "./Button.module.css";
+/**
+ *@param {Object} props
+ * @param {Boolean} [props.fullWidth] - Button full width
+ * @param {URL} [props.href] - href for the anchor element
+ * @param {'primary' | 'secondary'} [props.mode] - Button style
+ * @param {String} [props.text] - Button text
+ *
+ * @returns
+ */
+
+export const Button = ({ mode = "primary", text = "", fullWidth = false, href }) => {
+  return href ? (
+    <a className={styles.link} href={href} target="_blank" rel="noopener">
+      <button className={`${styles.button} ${styles[mode]}`}>{text}</button>
+    </a>
+  ) : (
+    <button className={`${styles.button} ${styles[mode]} ${fullWidth ? styles.fullWidth : ""}`}>{text}</button>
+  );
+};
