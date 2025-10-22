b687ef8adba9a2b7893b52a8a34aed48699397ca
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-22 16:25:28 -0300

    feat(Home): add skills section

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,15 @@
+import { Boostrap } from "./components/icons/Boostrap";
 import { Chat } from "./components/icons/Chat";
+import { CSS } from "./components/icons/CSS";
+import { Figma } from "./components/icons/Figma";
+import { Git } from "./components/icons/Git";
+import { JavaScript } from "./components/icons/JavaScript";
+import { NextJs } from "./components/icons/NextJs";
 import { Public } from "./components/icons/Public";
 import { Work } from "./components/icons/Work";
 import { Button } from "./components/ui/button/Button";
 import { InfoItem } from "./components/ui/infoItem/InfoItem";
+import { Skill } from "./components/ui/skill/Skill";
 
 export function Home() {
   return (
@@ -37,7 +44,14 @@ export function Home() {
       <section className="skills">
         <h2 className="sectionTitle">Mis Habilidades y Herramientas</h2>
         <p className="skillsDescription">Tecnologías que uso para dar vida a mis ideas</p>
-        <div className="skillsContainer"></div>
+        <div className="skillsContainer">
+          <Skill icon={NextJs} description="Next.js" />
+          <Skill icon={JavaScript} description="JavaScript" />
+          <Skill icon={Git} description={"Git"} />
+          <Skill icon={Figma} description={"Figma"} />
+          <Skill icon={CSS} description={"CSS"} />
+          <Skill icon={Boostrap} description={"Bootstrap"} />
+        </div>
       </section>
       <section className="contactMe">
         <h2 className="sectionTitle">Contáctame</h2>
@@ -46,7 +60,7 @@ export function Home() {
           <form className="contactForm">
             <input name="name" placeholder="Tu nombre"></input>
             <input name="email" placeholder="Tu email"></input>
-            <textarea name="message" Tu mensaje></textarea>
+            <textarea name="message" placeholder="Tu mensaje"></textarea>
           </form>
           <button>Enviar mensaje</button>
         </div>
