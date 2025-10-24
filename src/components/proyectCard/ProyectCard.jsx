dd5bbe5e4c18cd30dbfb9dc835958e9542e5b1f9
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-24 16:34:54 -0300

    feat(ProyectCard): make responsive and add new prop to reverse card layout

--- src/components/proyectCard/ProyectCard.jsx
+++ src/components/proyectCard/ProyectCard.jsx
@@ -10,18 +10,22 @@ import styles from "./ProyectCard.module.css";
  * @param {Array} props.tools - Array of tools used in the proyect
  * @param {String} props.proyectURL - Proyect page URL
  * @param {String} props.githubURL - Proyect repository URL
+ * @param {boolean} props.reverse - Reverse proyect card
  * @returns
  */
 
-export const ProyectCard = ({ title, description, img, tools, proyectURL, githubURL }) => {
+export const ProyectCard = ({ title, description, img, tools, proyectURL, githubURL, reverse = false }) => {
+  console.log(reverse);
   if (!title) return console.error("Se necesita pasar el título");
   if (!img) return console.error("Se necesita pasar el src de la imagen");
   if (!proyectURL) return console.error("Se necesita pasar la URL al proyecto");
   if (!githubURL) return console.error("Se necesita pasar la URL al github");
 
   return (
-    <div className={styles.proyectCard}>
-      <div className={styles.imgContainer}>{<img src={img} alt={`Imagen del proyecto ${title}`} />}</div>
+    <div className={`${styles.proyectCard} ${reverse ? styles.reverse : ""}`}>
+      <div className={styles.imgWrapper}>
+        <div className={styles.imgContainer}>{<img src={img} alt={`Imagen del proyecto ${title}`} />}</div>
+      </div>
       <div className={styles.body}>
         <p className={styles.title}>{title}</p>
         <p className={styles.description}>{description}</p>
