00561d5f5a6bcb022f2cdffbb296f1ef082719f4
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-10-27 18:12:54 -0300

    feat(infoItem): add className prop

--- src/components/ui/infoItem/InfoItem.jsx
+++ src/components/ui/infoItem/InfoItem.jsx
@@ -8,17 +8,17 @@ import styles from "./infoItem.module.css";
  * @returns
  */
 
-export const InfoItem = ({ icon: Icon, title, description }) => {
+export const InfoItem = ({ icon: Icon, title, description, className = "" }) => {
   return (
-    <div className={styles.infoItem}>
+    <div className={`${styles.infoItem} ${className}`}>
       {Icon ? (
-        <div className={styles.iconContainer}>
+        <div className={`${styles.iconContainer}`}>
           <Icon width="2rem" height="2rem" />
         </div>
       ) : (
         ""
       )}
-      <div className={styles.infoContainer}>
+      <div className={`${styles.infoContainer} `}>
         <p className={styles.title}>{title}</p>
         <p className={styles.description}>{description}</p>
       </div>
